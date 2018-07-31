e9cbde6042df68a245bf089ce1f8929976552ce5
Bo <bogarrettcameron@gmail.com>
2018-07-30 23:58:35 -0400

    Machine reservation now checks if the reservation is valid, and the user can only choose the time with dropdown boxes now.

--- app/database.py
+++ app/database.py
@@ -1,124 +1,193 @@
-#database for users
+# database for users
 
 import shelve
 import re
 import requests
 
-#database that saves the users profile and their info
+
+# database that saves the users profile and their agenda
 
 def userAcct(name, user, password):
-	print("connected")
-	try:
-		print("trying")
-		data = shelve.open('acct.db', writeback = True)
-		#if this user is not a new user, fail
-		if user in data:
-			data.close()
-			print('failure')
-			return "failure"
-		#else create the password and user
-		else:
-			print("IN DB")
-			#data = {user : [{'name' : name, 'password' : password}]}
-			book = []
-			interviewList = []
-			data[user] = {'name' : name, 'password' : password, 'machines' : book, 'onePlan' : plan}
-	except KeyError:
-		return "KeyError"
+    print("connected")
+    try:
+        print("trying")
+        data = shelve.open('acct.db', writeback=True)
+        # if this user is not a new user, fail
+        if user in data:
+            data.close()
+            print('failure')
+            return "failure"
+        # else create the password and user
+        else:
+            print("IN DB")
+            # data = {user : [{'name' : name, 'password' : password}]}
+            book = []
+            interviewList = []
+            data[user] = {'name': name, 'password': password, 'machines': book, 'interview': interviewList}
+    except KeyError:
+        return "KeyError"
 
-	data.close()
-
-
-def bookMachine(user, clientName, machineType, date, timeStart, timeEnd, slot, info):
-	try:
-		#open the database
-		data = shelve.open('acct.db', writeback = True)
-		#set the list to the user's existing list if it exists
-		book = data[user]['machines']
-
-		if not book:
-			num = 1
-		else:
-			num = book[-1]['id'] + 1
-
-		print ("NUM: ", num)
-
-		#store the information in a dict
-		machines = {'id' : num,
-				  'clientName' : clientName,
-			  	  'machineType' : machineType,
-				  'date' : date,
-				  'timeStart' : timeStart,
-			  	  'timeEnd' : timeEnd,
-			      'slot' : slot,
-			  	  'info' : info}
-
-		print machines
-		
-		book.append(machines)
-		#update it in the database
-		data[user]['machines'] = book
-
-		data.close()
-		print "booking successful"
-		return "Created successfully"
-	except:
-		print "booking failure"
-		return "Failed to create"
-
-def workoutPlan(user, month, day, year, areas, machines, types, slot, info):
-	try:
-		#open the database
-		data = shelve.open('acct.db', writeback = True)
-		#set the list to the user's existing list if it exists
-		plan = data[user]['plan']
-
-		if not plan:
-			num = 1
-		else:
-			num = plan[-1]['id'] + 1
-
-		print ("NUM: ", num)
-
-		
-		onePlan = {'id' : num,
-				  'month' : month,
-				  'day' : day,
-			  	  'year' : year,
-				  'areas' : areas,
-				  'machines' : machines,
-				  'types' : types,
-			  	  'slot' : slot,
-			      'info' : info}
-
-		print onePlan
-		
-		plan.append(onePlan)
-		#update it in the database
-		data[user]['onePlan'] = plan
-
-		data.close()
-		print "plan successful"
-		return "Created successfully"
-	except:
-		print "plan failure"
-		return "Failed to create"
+    data.close()
 
 
+def bookMachine(user, clientName, machineType, date, hourStart, minuteStart, ampmStart, slot, info):
+#    try:
+        print("starting try")
+        # get our times calculated
+        timeStart = time12to24(hourStart, minuteStart, ampmStart)
+        print("timeStart = ", timeStart)
+        timeEnd = endTime(timeStart, slot)
+        print("timeEnd = ", timeEnd)
+
+        # open the database
+        data = shelve.open('acct.db', writeback=True)
+
+        # check if the date and time is valid
+        if not machineOpen(timeStart, timeEnd, data, machineType):
+            print("booking failed: unavailable date/time")
+            return "Invalid Booking Time"
+        else:
+            # set the list to the user's existing list if it exists
+            book = data[user]['machines']
+
+            if not book:
+                num = 1
+            else:
+                num = book[-1]['id'] + 1
+
+            print ("NUM: ", num)
+
+            # store the agenda information in a dict
+            machines = {'id': num,
+                        'clientName': clientName,
+                        'machineType': machineType,
+                        'date': date,
+                        'timeStart': timeStart,
+                        'timeEnd': timeEnd,
+                        'slot': slot,
+                        'info': info}
+
+            print machines
+            # append to the agenada list
+            book.append(machines)
+            # update it in the database
+            data[user]['machines'] = book
+
+            data.close()
+            print "booking successful"
+            return "Created successfully"
+#    except:
+        print "booking failure"
+        return "Failed to create"
+
+
+def machineOpen(timeStart, timeEnd, db, machineType):
+    for user in db:
+        try:
+            myMachines = db[user]['machines']
+            #print db[user]['name']
+            for booking in myMachines:
+                tmpStart = int(booking.get('timeStart'))
+                tmpEnd = int(booking.get('timeEnd'))
+                #print booking
+                if booking['machineType'] == machineType and timesOverlap(timeStart, timeEnd, tmpStart, tmpEnd):
+                    return False
+        except:
+            print(db[user]['name'], "this is an old account in the database")
+    return True
+
+
+def timesOverlap(start1, end1, start2, end2):
+    if (start2 < start1 < end2) or (start2 < end1 < end2)\
+            or (start1 < start2 < end1) or (start1 < end2 < end2)\
+            or (start1 == start2) or (end1 == end2):
+        return True
+    else:
+        return False
+
+
+# we won't allow anyone to have a slot that hits 12 am
+def validSlot(timeStart, slot):
+    return endTime(timeStart, slot) < 2400
+
+
+def time12to24(hour, minute, amPm):
+    if amPm == "PM" and int(hour) != 12:
+        mhour = int(hour) + 12
+    elif amPm == "AM" and int(hour) == 12:
+        mhour = 24
+    else:
+        mhour = int(hour)
+    return mhour * 100 + int(minute)
+
+
+# this will work as long as we keep slot lengths limited to 45
+def endTime(timeStart, slot):
+    rem = timeStart % 100 + int(slot)
+    while rem > 60:
+        timeStart += 100    # add an hour
+        rem -= 60           # adjust minutes
+    return timeStart + rem
+
+
+def usrInterviewQA(user, company, yourself, goals, why, want, expecting, strweak, leave, describe, situation, position,
+                   decision, questions):
+    try:
+        # open the database
+        data = shelve.open('acct.db', writeback=True)
+        # set the list to the user's existing list if it exists
+        interviewList = data[user]['interview']
+
+        if not interviewList:
+            num = 100
+        else:
+            num = interviewList[-1]['id'] + 1
+
+        print ("NUM: ", num)
+
+        # store the agenda information in a dict
+        interview = {'id': num,
+                     'company': company,
+                     'yourself': yourself,
+                     'goals': goals,
+                     'why': why,
+                     'want': want,
+                     'expecting': expecting,
+                     'strweak': strweak,
+                     'leave': leave,
+                     'describe': describe,
+                     'situation': situation,
+                     'position': position,
+                     'decision': decision,
+                     'questions': questions}
+
+        print interview
+        # append to the agenada list
+        interviewList.append(interview)
+        # update it in the database
+        data[user]['interview'] = interviewList
+
+        data.close()
+        print "interview successful"
+        return "Created successfully"
+    except:
+        print "interview failure"
+        return "Failed to create"
 
 
 def getMBooking(user):
-	data = shelve.open('acct.db', writeback = True)
-	book = data[user]['machines']
-	print book
+    data = shelve.open('acct.db', writeback=True)
+    book = data[user]['machines']
+    print book
 
-	data.close()
-	return book
+    data.close()
+    return book
 
-def getPlan(user):
-        data = shelve.open('acct.db', writeback = True)
-        plan = data[user]['onePlan']
-        print plan
 
-        data.close()
-        return plan
+def getInterview(user):
+    data = shelve.open('acct.db', writeback=True)
+    interviewList = data[user]['interview']
+    print interviewList
+
+    data.close()
+    return interviewList
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import b_app
-from app.database import userAcct, bookMachine, getMBooking, workoutPlan, getPlan
-#, usrAgenda, getAgenda, usrInterviewQA, getInterview
+from app.database import userAcct, bookMachine, getMBooking
+# , usrAgenda, getAgenda, usrInterviewQA, getInterview
 import requests
 import re
 import shelve
@@ -10,13 +10,15 @@ loggedIn = False
 currUser = ""
 email = ""
 
-#implementation of the added user
+
+# implementation of the added user
 
 @b_app.context_processor
 def addUser():
-  return dict(acct=currUser)
+    return dict(acct=currUser)
 
-#Route for the index or home page
+
+# Route for the index or home page
 @b_app.route('/', methods=['GET', 'POST'])
 def home():
     pw = ""
@@ -25,28 +27,28 @@ def home():
     print "in register"
     print (request.method)
 
-    #if the user is logged in, do not allow them to go back to register
+    # if the user is logged in, do not allow them to go back to register
     print currUser
     if currUser != "":
         return redirect(url_for('index'))
 
     print(request.method)
     if request.method == 'POST':
-        #if the sign in button was pressed
+        # if the sign in button was pressed
         data = request.form['signSubmit']
         if request.form['signSubmit'] == 'submitted':
-            s = shelve.open('acct.db')	#open the database
-            #check if the account has been created, where user_key is the email
+            s = shelve.open('acct.db')  # open the database
+            # check if the account has been created, where user_key is the email
             try:
                 print("trying...")
                 if str(request.form.get('user_key')) in s:
-                    #check to see if password matches the user's actual password
+                    # check to see if password matches the user's actual password
                     if s[str(request.form.get('user_key'))]['password'] == str(request.form.get('user_password')):
                         loggedIn = True
                         currUser = s[str(request.form.get('user_key'))]['name']
                         email = str(request.form.get('user_key'))
                         return redirect(url_for('index'))
-					#if not failure, display output
+                    # if not failure, display output
                     else:
                         pw = "Invalid credentials. Incorrect password or email."
                         return render_template('register.html', pw=pw)
@@ -55,20 +57,20 @@ def home():
                     return render_template('register.html', pw=pw)
 
             except KeyError:
-				print "Account not registered"
+                print "Account not registered"
         elif request.form['signSubmit'] == 'signUpSubmit':
             print("in signup")
-            #get the name, acct, password for the user when they sign up
+            # get the name, acct, password for the user when they sign up
             name = str(request.form.get('username'))
             acct = str(request.form.get('useremail'))
             pw = str(request.form.get('userpass'))
-            #store it in the database if successful
+            # store it in the database if successful
             message = userAcct(name, acct, pw)
-            #if the database returns a failure
+            # if the database returns a failure
             if message == 'failure':
                 err = "Email is already taken. Please try again"
                 return render_template("register.html", err=err)
-            #check for a valid email format
+            # check for a valid email format
             elif re.search("@([0-9a-z][0-9a-z-]+.)+[a-z]{2,4}$", acct) is None:
                 err = "Please enter a valid email address"
                 print "Invalid Email Format"
@@ -77,7 +79,7 @@ def home():
                 err = "Password is too short (minimum of 7 characters)"
                 print "short password"
                 return render_template("register.html", err=err)
-            #if no error take them to index page
+            # if no error take them to index page
             else:
                 err = ""
                 loggedIn = True
@@ -87,18 +89,19 @@ def home():
                 email = acct
                 return redirect(url_for('index'))
 
-
-    #create the view for register.html
+    # create the view for register.html
     return render_template('register.html', err=err)
 
-#each line below connects the html page to the views.py page to redirect the links from the home page
+
+# each line below connects the html page to the views.py page to redirect the links from the home page
 @b_app.route('/index.html', methods=['GET', 'POST'])
 def index():
     if currUser == "":
         return redirect(url_for('home'))
 
-    #create the view for the index
+    # create the view for the index
     return render_template('index.html')
+
 
 @b_app.route('/machines.html', methods=['GET', 'POST'])
 def newbooking():
@@ -110,45 +113,30 @@ def newbooking():
     info = None
 
     if request.method == 'POST':
-	        clientName = request.form.get('clientName')
-	        machineType = request.form.get('machineType')
-	        date = request.form.get('date')
-	        timeStart = request.form.get('timeStart')
-		timeEnd = request.form.get('timeEnd')
-	        slot = request.form.get('slot')
-	        info = request.form.get('info')
-	
-	
-	        alert = bookMachine(email, clientName, machineType, date, timeStart, timeEnd, slot, info)
-	        return redirect(url_for('profile', alert=alert))
+        clientName = request.form.get('clientName')
+        machineType = request.form.get('machineType')
+        date = request.form.get('date')
+        #timeStart = request.form.get('timeStart')
+        #timeEnd = request.form.get('timeEnd')
+        hourStart = request.form.get('hourStart')
+        minuteStart = request.form.get('minuteStart')
+        ampmStart = request.form.get('ampmStart')
+        slot = request.form.get('slot')
+        info = request.form.get('info')
+
+        alert = bookMachine(email, clientName, machineType, date, hourStart, minuteStart, ampmStart, slot, info)
+        return redirect(url_for('profile', alert=alert))
 
     return render_template('machines.html')
 
 
 @b_app.route('/plan.html', methods=['GET', 'POST'])
-def newplan():
+def newthing():
     if currUser == "":
         return redirect(url_for('home'))
 
-    print "for new plan"
-    print (request.method)
-    info = None
-
-    if request.method == 'POST':
-	        month = request.form.get('month')
-	        day = request.form.get('day')
-	        year = request.form.get('year')
-	        areas = request.form.get('areas')
-		machines = request.form.get('machines')
-	        types = request.form.get('types')
-	        slot = request.form.get('slot')
-	        info = request.form.get('info')
-	
-	
-	        alert = workoutPlan(email, month, day, year, areas, machines, types, slot, info)
-	        return redirect(url_for('profile', alert=alert))
-
     return render_template('plan.html')
+
 
 @b_app.route('/here.html', methods=['GET', 'POST'])
 def newinterview():
@@ -157,14 +145,16 @@ def newinterview():
 
     return render_template('here.html')
 
+
 @b_app.route('/weekcal.html')
 def newdate():
-    #print currUser
+    # print currUser
     if currUser == "":
         return redirect(url_for('home'))
 
-    #machines = getMBooking(email)
+    # machines = getMBooking(email)
     return render_template('weekcal.html')
+
 
 @b_app.route('/trainers.html')
 def newlocation():
@@ -174,13 +164,13 @@ def newlocation():
     return render_template('trainers.html')
 
 
-
 @b_app.route('/about.html')
 def aboutus():
     if currUser == "":
         return redirect(url_for('home'))
 
     return render_template('about.html')
+
 
 @b_app.route('/signout.html')
 def signout():
@@ -191,6 +181,7 @@ def signout():
 
     return render_template('signout.html')
 
+
 @b_app.route('/profile.html')
 def profile():
     print currUser
@@ -198,4 +189,3 @@ def profile():
         return redirect(url_for('home'))
 
     return render_template('profile.html')
-
